c3f91f2dd33d4a9bc43daa1305c8038a0cf44cd0
mern-project01 <mernp29@gmail.com>
2025-03-20 22:26:07 +0600

    mongdb conection

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const app = express();
+const port = 4000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+require("dotenv").config();
+
+
+
+app.get("/", (req, res) => {
+  res.send("app runing");
+});
+app.get("/users", (req, res) => {
+  res.send("user rout");
+});
+// mongodb conection
+
+
+const uri =`mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASS}@cluster0.3aawf.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+
+async function run() {
+  try {
+  
+    await client.connect();
+    
+   const database= await client.db("Afer_fail");
+    const appontmentOption_collection = database.collection("appontmentOption");
+
+    app.get("/appontmentOption",async (req, res) => {
+      const result = await appontmentOption_collection.find({}).toArray()
+      res.send(result)
+    });
+   
+    
+    
+    console.log("conected");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// end mongodb conection
+app.listen(port, () => {
+  console.log(`exlample app listening on port ${port}`);
+});
