4d2cde6ae0ee05e202b9cdebe4929e6d43693b35
mern-project01 <mernp29@gmail.com>
2025-04-27 21:57:12 +0600

    add booking

--- index.js
+++ index.js
@@ -7,13 +7,145 @@ app.use(cors());
 
 require("dotenv").config();
 
-
+app.use(express.json())
 
 app.get("/", (req, res) => {
-  res.send("app runing");
-});
-app.get("/users", (req, res) => {
-  res.send("user rout");
+  // const data = [
+  //   {
+  //     _id: 1,
+  //     name: "Teeth Orthodontics",
+  //     slots: [
+  //       "08.00 AM - 08.30 AM",
+  //       "08.30 AM - 09.00 AM",
+  //       "09.00 AM - 9.30 AM",
+  //       "09.30 AM - 10.00 AM",
+  //       "10.00 AM - 10.30 AM",
+  //       "10.30 AM - 11.00 AM",
+  //       "11.00 AM - 11.30 AM",
+  //       "11.30 AM - 12.00 AM",
+  //       "1.00 PM - 1.30 PM",
+  //       "1.30 PM - 2.00 PM",
+  //       "2.00 PM - 2.30 PM",
+  //       "2.30 PM - 3.00 PM",
+  //       "3.00 PM - 3.30 PM",
+  //       "3.30 PM - 4.00 PM",
+  //       "4.00 PM - 4.30 PM",
+  //       "4.30 PM - 5.00 PM",
+  //     ],
+  //   },
+  //   {
+  //     _id: 2,
+  //     name: "Cosmetic Dentistry",
+  //     slots: [
+  //       "08.00 AM - 08.30 AM",
+  //       "08.30 AM - 09.00 AM",
+  //       "09.00 AM - 9.30 AM",
+  //       "09.30 AM - 10.00 AM",
+  //       "10.00 AM - 10.30 AM",
+  //       "10.30 AM - 11.00 AM",
+  //       "11.00 AM - 11.30 AM",
+  //       "11.30 AM - 12.00 AM",
+  //       "1.00 PM - 1.30 PM",
+  //       "1.30 PM - 2.00 PM",
+  //       "2.00 PM - 2.30 PM",
+  //       "2.30 PM - 3.00 PM",
+  //       "3.00 PM - 3.30 PM",
+  //       "3.30 PM - 4.00 PM",
+  //       "4.00 PM - 4.30 PM",
+  //       "4.30 PM - 5.00 PM",
+  //     ],
+  //   },
+  //   {
+  //     _id: 3,
+  //     name: "Teeth Cleaning",
+  //     slots: [
+  //       "08.00 AM - 08.30 AM",
+  //       "08.30 AM - 09.00 AM",
+  //       "09.00 AM - 9.30 AM",
+  //       "09.30 AM - 10.00 AM",
+  //       "10.00 AM - 10.30 AM",
+  //       "10.30 AM - 11.00 AM",
+  //       "11.00 AM - 11.30 AM",
+  //       "11.30 AM - 12.00 AM",
+  //       "1.00 PM - 1.30 PM",
+  //       "1.30 PM - 2.00 PM",
+  //       "2.00 PM - 2.30 PM",
+  //       "2.30 PM - 3.00 PM",
+  //       "3.00 PM - 3.30 PM",
+  //       "3.30 PM - 4.00 PM",
+  //       "4.00 PM - 4.30 PM",
+  //       "4.30 PM - 5.00 PM",
+  //     ],
+  //   },
+  //   {
+  //     _id: 4,
+  //     name: "Cavity Protection",
+  //     slots: [
+  //       "08.00 AM - 08.30 AM",
+  //       "08.30 AM - 09.00 AM",
+  //       "09.00 AM - 9.30 AM",
+  //       "09.30 AM - 10.00 AM",
+  //       "10.00 AM - 10.30 AM",
+  //       "10.30 AM - 11.00 AM",
+  //       "11.00 AM - 11.30 AM",
+  //       "11.30 AM - 12.00 AM",
+  //       "1.00 PM - 1.30 PM",
+  //       "1.30 PM - 2.00 PM",
+  //       "2.00 PM - 2.30 PM",
+  //       "2.30 PM - 3.00 PM",
+  //       "3.00 PM - 3.30 PM",
+  //       "3.30 PM - 4.00 PM",
+  //       "4.00 PM - 4.30 PM",
+  //       "4.30 PM - 5.00 PM",
+  //     ],
+  //   },
+  //   {
+  //     _id: 5,
+  //     name: "Pediatric Dental",
+  //     slots: [
+  //       "08.00 AM - 08.30 AM",
+  //       "08.30 AM - 09.00 AM",
+  //       "09.00 AM - 9.30 AM",
+  //       "09.30 AM - 10.00 AM",
+  //       "10.00 AM - 10.30 AM",
+  //       "10.30 AM - 11.00 AM",
+  //       "11.00 AM - 11.30 AM",
+  //       "11.30 AM - 12.00 AM",
+  //       "1.00 PM - 1.30 PM",
+  //       "1.30 PM - 2.00 PM",
+  //       "2.00 PM - 2.30 PM",
+  //       "2.30 PM - 3.00 PM",
+  //       "3.00 PM - 3.30 PM",
+  //       "3.30 PM - 4.00 PM",
+  //       "4.00 PM - 4.30 PM",
+  //       "4.30 PM - 5.00 PM",
+  //     ],
+  //   },
+  //   {
+  //     _id: 6,
+  //     name: "Oral Surgery",
+  //     slots: [
+  //       "08.00 AM - 08.30 AM",
+  //       "08.30 AM - 09.00 AM",
+  //       "09.00 AM - 9.30 AM",
+  //       "09.30 AM - 10.00 AM",
+  //       "10.00 AM - 10.30 AM",
+  //       "10.30 AM - 11.00 AM",
+  //       "11.00 AM - 11.30 AM",
+  //       "11.30 AM - 12.00 AM",
+  //       "1.00 PM - 1.30 PM",
+  //       "1.30 PM - 2.00 PM",
+  //       "2.00 PM - 2.30 PM",
+  //       "2.30 PM - 3.00 PM",
+  //       "3.00 PM - 3.30 PM",
+  //       "3.30 PM - 4.00 PM",
+  //       "4.00 PM - 4.30 PM",
+  //       "4.30 PM - 5.00 PM",
+  //     ],
+  //   },
+  // ];
+
+  res.send('app is runnig');
 });
 // mongodb conection
 
@@ -36,14 +168,38 @@ async function run() {
     
    const database= await client.db("Afer_fail");
     const appontmentOption_collection = database.collection("appontmentOption");
-
+    const booking_appointment = database.collection("booking");
     app.get("/appontmentOption",async (req, res) => {
       const result = await appontmentOption_collection.find({}).toArray()
       res.send(result)
     });
-   
+    // app.post("/booking", async (req, res) => {
+    //   const booking_info = await req.body;
+    //   // booking_appointment.insertOne(booking_info);
+    //  await booking_appointment.insertOne(booking_appointment)
+    //   console.log(booking_info);
+
     
-    
+    // });
+    // app.get('/booking', async (req, res) => {
+    //         const result = await booking_appointment.find({}).toArray();
+    //       res.send(result)
+    // })
+    app.post("/booking", async (req, res) => {
+      const booking_info = req.body; // no need await here
+      const result = await booking_appointment.insertOne(booking_info); // insert booking_info
+      console.log(booking_info);
+      res.send(result);
+    });
+
+    // (Optional) if you want to add GET /booking to fetch all bookings
+    app.get("/booking", async (req, res) => {
+      const result = await booking_appointment.find({}).toArray();
+      console.log(result)
+      res.send(result);
+    });
+
+
     console.log("conected");
   } finally {
     // await client.close();
@@ -52,6 +208,7 @@ async function run() {
 run().catch(console.dir);
 
 // end mongodb conection
+
 app.listen(port, () => {
   console.log(`exlample app listening on port ${port}`);
 });
